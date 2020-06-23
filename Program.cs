76d53cd47079f2f836c92008ad524953ecc504a2
Muhmen777 <58171494+Muhmen777@users.noreply.github.com>
2020-06-23 12:58:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using PointAndLines;
+
+namespace OOPPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User(); //Nyt objekt
+
+            user.Username = "Abcde"; //tildeler
+            System.Console.WriteLine(user.Username); //udskriver
+            user.Password = 21221;
+            System.Console.WriteLine(user.Password);
+
+        }
+    }
+}
